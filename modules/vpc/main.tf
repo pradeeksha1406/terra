edc5e90aea7126ff8baf235bb03a6577f64531a2
pradeep <p.guta@gmail.com>
2024-12-03 17:17:50 +0530

    code structure

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "main" {
 
   }
 }
+
+resource "aws_subnet" "public_subnets" {
+  count = length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+
+    Name = "public-subnet-${count.index}"
+  }
+}
