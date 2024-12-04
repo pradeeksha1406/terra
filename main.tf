0210c742bf7c621b9249c6071d59e3ceaefaefc6
pradeep <p.guta@gmail.com>
2024-12-04 11:13:28 +0530

    code structure

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   vpc_cidr = var.vpc_cidr
   env =var.env
   public_subnets = var.public_subnets
-  private_subnets = var.public_subnets
+  private_subnets = var.private_subnets
   azs = var.azs
 }
 
