994f9759196c1335ef0e42611bc61507b08cefe5
pradeep <p.guta@gmail.com>
2024-12-04 11:11:17 +0530

    code structure

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -27,3 +27,25 @@ resource "aws_subnet" "private_subnets" {
     Name = "private-subnet-${count.index}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+resource "aws_eip" "ngw" {
+  domain ="vpc"
+
+}
+
+#resource "aws_nat_gateway" "igw" {
+#  allocation_id = aws_eip.ngw.id
+#  subnet_id     = aws_subnet.public_subnets
+#
+#  tags = {
+#    Name = "${var.env}-ngw"
+#  }
+#}
