5b577dd1a8f82e477a4de4ab7aa6b4592b810322
pradeep <p.guta@gmail.com>
2024-12-10 09:29:56 +0530

    code structure

--- env-dev/inputs.tfvars
+++ env-dev/inputs.tfvars
@@ -6,5 +6,6 @@ private_subnets = ["10.0.2.0/24" , "10.0.3.0/24"]
 azs =["us-east-1a" , "us-east-1b"]
 account_no = "180294219212"
 default_vpc_id = "vpc-0c9b02891a05de341"
+default_vpc_cidr =""
 
 
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -87,6 +87,12 @@ resource "aws_route_table" "private" {
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+
+  }
+
   tags = {
     Name = "private"
   }
