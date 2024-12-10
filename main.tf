5515a8bf2b8fc988f32601ca44a628b448ead2c9
pradeep <p.guta@gmail.com>
2024-12-10 14:00:22 +0530

    code structure

--- main.tf
+++ main.tf
@@ -31,4 +31,14 @@ module "private-alb" {
   vpc_id                  = module.vpc.vpc_id
 }
 
+module "frontend" {
+  source = "./modules/app"
+  app_port = "80"
+  component = "frontend"
+  env = var.env
+  instance_type = "t3.micro"
+  vpc_cidr = var.vpc_cidr
+  vpc_id = module.vpc.vpc_id
+}
+
 
