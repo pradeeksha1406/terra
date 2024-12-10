941bf9144141af2b3a1a6a3583dc9965ce0f99c6
pradeep <p.guta@gmail.com>
2024-12-10 10:57:51 +0530

    code structure

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -83,7 +83,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/24"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
