9c007751470bc5134d18794a6071fb3d4422c606
pradeep <p.guta@gmail.com>
2024-12-16 12:18:39 +0530

    code structure

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-dnf install python3.11-pip -y
-pip3.11 install boto3 botocore
+#dnf install python3.11-pip -y
+#pip3.11 install boto3 botocore
+
+sudo dnf install nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
