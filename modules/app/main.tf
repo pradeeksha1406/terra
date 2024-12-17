8e091117990f2b6a1471f9728dad62748fd3481e
pradeep <p.guta@gmail.com>
2024-12-17 11:02:42 +0530

    code structure

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -78,6 +78,11 @@ resource "aws_iam_role" "role" {
   }
 }
 
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
 
 resource "aws_launch_template" "template" {
   name                   = "${var.env}-${var.component}"
