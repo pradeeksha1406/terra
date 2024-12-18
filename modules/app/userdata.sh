4398139b20b3274936cdf793c40bcdaab797ebc8
pradeep <p.guta@gmail.com>
2024-12-18 16:16:28 +0530

    code structure

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -2,6 +2,6 @@
 
 dnf install python3.12-pip -y | tee -a /opt/userdata.log
 pip3.12 install boto3 botocore | tee -a /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/pradeeksha1406/Revise-infra-ansible.git main.yml -e role_name=${role_name} | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/pradeeksha1406/Revise-infra-ansible.git main.yml -e role_name=${role_name} -e env={{env}} | tee -a /opt/userdata.log
 
 
